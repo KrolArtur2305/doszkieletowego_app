db89a9e8ee57b136ffe03acfd8335d23c0c98f87
Artur Szczepanik <artur.szczepanik23@gmail.com>
2025-11-05 23:12:40 +0100

    fix(mobile): add web-streams + URL polyfills and wire before router entry (SDK54)

--- App.tsx
+++ App.tsx
@@ -1,1 +1,2 @@
+﻿import "./app/polyfills";
 import "expo-router/entry";
---
+++ app/polyfills.ts
@@ -0,0 +1,7 @@
+﻿import "react-native-url-polyfill/auto";
+
+// Metro próbuje rozwiązać ten import – jeśli paczki nie ma, sypie się bundling.
+// Mamy ją w deps, ale na wszelki wypadek wrap w try/catch.
+try {
+  require("web-streams-polyfill/ponyfill/es6");
+} catch {}
